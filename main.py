330765995ed7cd65f7cc68a5aec91cd434493675
Simon Walker <github@stwalkerster.co.uk>
2023-01-03 21:41:53 +0000

    Logging configuration

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import os
 import json
 import pika
 import ssl
+import datetime
 
 aws_session_token = os.environ.get('AWS_SESSION_TOKEN')
 rabbit_base_path = os.environ.get('RABBIT_BASE')
@@ -15,7 +16,6 @@ hmac_path = os.environ.get('TFE_HMAC')
 rabbit_channel = None
 rabbit_exchange = ""
 
-logging.basicConfig(format='[%(levelname)s] %(message)s', level=logging.DEBUG)
 
 def get_secret(path):
     safe_parameter = urllib.parse.quote(path, safe="")
@@ -32,13 +32,14 @@ def rabbit(lambda_context):
     global rabbit_exchange
 
     if rabbit_channel is not None:
-        logging.info("Channel already defined; reusing...")
-        return
+        if rabbit_channel.is_open:
+            logging.info("Channel already defined; reusing...")
+            return
 
-    logging.debug("Fetching RabbitMQ configuration")
+    logging.info("Fetching RabbitMQ configuration")
     params = get_rabbit_config()
 
-    logging.debug("Setting up RabbitMQ connection")
+    logging.info("Setting up RabbitMQ connection")
     ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
     ssl_context.verify_mode = ssl.CERT_REQUIRED
     ssl_context.check_hostname = True
@@ -63,9 +64,14 @@ def get_rabbit_config():
 
 
 def handler(event, context):
-    logging.debug("Lambda request ID:", context.aws_request_id)
+    logging.getLogger().setLevel(logging.INFO)
+    timestamp = datetime.datetime.utcnow().isoformat()
+
+    logging.info("Lambda request ID: %s", context.aws_request_id)
 
     hmac_key = get_secret(hmac_path)
+
+    logging.info("Checking headers")
 
     for header in ["hdr", "sig", "prefix", "dest", "src"]:
         if header not in event["queryStringParameters"]:
@@ -81,21 +87,21 @@ def handler(event, context):
         return json.dumps(dict(statusCode=403, isBase64Encoded=False, body="No signature detected."))
 
     received_signature = event["headers"][event["queryStringParameters"]["hdr"]]
-    hash_type = hashes[event["queryStringParameters"]["sig"]]
-    hash_prefix = event["queryStringParameters"]["prefix"]
-
     event_body = event["body"]
 
-    calculated_signature = "{0}{1}".format(hash_prefix,
-                                           hmac.new(hmac_key.encode(), event_body.encode(), hash_type).hexdigest())
+    logging.info("Checking checksum")
+
+    calculated_signature = "{0}{1}".format(event["queryStringParameters"]["prefix"],
+                                           hmac.new(hmac_key.encode(), event_body.encode(),
+                                                    hashes[event["queryStringParameters"]["sig"]]).hexdigest())
 
     if received_signature != calculated_signature:
-        logging.error("Mismatched HMAC", received_signature, calculated_signature)
+        logging.error("Mismatched HMAC! recvd: %s, calc: %s", received_signature, calculated_signature)
         return json.dumps(dict(statusCode=403, isBase64Encoded=False, body="Mismatched HMAC"))
 
     rabbit(context)
 
-    logging.debug("Publishing to RabbitMQ")
+    logging.info("Publishing to RabbitMQ")
     msg_headers = {
         "aws_function_arn": context.invoked_function_arn,
         "aws_request_id": context.aws_request_id,
@@ -103,6 +109,7 @@ def handler(event, context):
         "destination": event["queryStringParameters"]["dest"],
         "source": event["queryStringParameters"]["src"],
         "hmac": calculated_signature,
+        "timestamp": timestamp
     }
     msg_props = pika.BasicProperties(content_type="application/json", headers=msg_headers)
     rabbit_channel.basic_publish(exchange=rabbit_exchange, routing_key=event["queryStringParameters"]["dest"],
